d63f39895e39aa2681162ebd37956f6931a44dcf
ivloa6919@gmail.com <ivloa6919@gmail.com>
2022-10-05 22:08:35 -0500

    Version 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -69,12 +69,25 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+switch (color) {
+  case "blue" :
+    return "This is blue";
+  case "red" :
+    return "This is red";
+  case "green" :
+    return "This is green";
+  case "orange" :
+    return "This is orange";
+    default :
+    return "Color not found";
+}
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero === 10 || numero === 5;
 }
 
 function estaEnRango(numero) {
